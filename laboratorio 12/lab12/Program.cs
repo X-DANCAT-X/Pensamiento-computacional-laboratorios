3a3707054518ceddb7ba25331360eaaf5dd32a8b
Jdan <75763350+X-DANCAT-X@users.noreply.github.com>
2025-05-07 23:17:46 -0600

    Add files via upload

---
+++ laboratorio 12/lab12/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Write("Ingrese un texto:");
+        string input = Console.ReadLine().Trim();
+        int conteopalabra = 0;
+        bool enPalabra = false;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (char.IsWhiteSpace(input[i]))
+            {
+                enPalabra = false;
+            }
+            else if (!enPalabra)
+            {
+                enPalabra = true;
+                conteopalabra++;
+            }
+        }
+
+        // Capitalizar la primera letra de cada palabra
+        char[] caracteres = input.ToLower().ToCharArray();
+        bool nuevaPalabra = true;
+
+        for (int i = 0; i < caracteres.Length; i++)
+        {
+            if (char.IsWhiteSpace(caracteres[i]))
+            {
+                nuevaPalabra = true;
+            }
+            else if (nuevaPalabra)
+            {
+                caracteres[i] = char.ToUpper(caracteres[i]);
+                nuevaPalabra = false;
+            }
+        }
+
+        string capitalizedText = new string(caracteres);
+
+        Console.WriteLine($"Cantidad de palabras: {conteopalabra}");
+        Console.WriteLine($"Texto modificado: {capitalizedText}");
+        

04e5454f13c7e9fa33c004a3b5c377ea80d78d07
Jdan <75763350+X-DANCAT-X@users.noreply.github.com>
2025-05-07 22:42:01 -0600

    Add files via upload

---
+++ laboratorio 8/Laboratorio8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Bienvenido,Ingrese su nombre completo: ");
+string nombre = Console.ReadLine();
+
+Console.Write("Ingrese su altura en metros: ");
+float altura = float.Parse(Console.ReadLine());
+
+Console.Write("Ingrese su edad: ");
+int edad = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese la inicial de su mascota: ");
+char inicial = char.Parse(Console.ReadLine());
+       
+Console.WriteLine("Impresion de datos:");
+Console.WriteLine(nombre);
+Console.WriteLine(altura);
+Console.WriteLine(edad);
+Console.WriteLine(inicial);

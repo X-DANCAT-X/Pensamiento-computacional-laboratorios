918efcfa479bc7631b0610151257d61262405b4f
Jdan <75763350+X-DANCAT-X@users.noreply.github.com>
2025-05-07 23:22:29 -0600

    Add files via upload

---
+++ laboratorio 14/lab 14/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Listado estudiantes y sus notas");
+
+string[] estudiantes = { "Juan", "Pedro", "Luisa", "Adriana", "Sofia" };
+int[] notas = { 88, 75, 96, 77, 59 };
+int suma = 0;
+
+Console.WriteLine("------------------------------------------------------------------");
+for (int i = 0; i < estudiantes.Length; i++)
+{
+    Console.WriteLine($"{estudiantes[i]} - {notas[i]}");
+    suma + = notas[i];
+}
+    double promedio = (double)suma / estudiantes.Length;
+    Console.WriteLine();
+    Console.WriteLine("El promedio es: " + promedio);
+    Console.WriteLine("------------------------------------------------------------------");
+
